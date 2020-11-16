80b8bcb5dfcdb5d7898fd14be75ed8b6cb9880eb
Cooper Cone <coopercone@yahoo.com>
2020-11-16 16:03:01 -0500

    Added headers

--- src/main.c
+++ src/main.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
+
 
 int main(int argc, char **argv) {
   printf("Hello world\n");
