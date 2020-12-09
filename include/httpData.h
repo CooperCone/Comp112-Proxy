d0c9a092272e2ecf74579fa07c133486f082642b
Cooper Cone <coopercone@yahoo.com>
2020-12-08 19:25:58 -0700

    Bug fixes and commented lists

--- include/httpData.h
+++ include/httpData.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stdbool.h>
+#include <time.h>
 #include "dynamicArray.h"
 
 typedef enum {
@@ -69,14 +70,35 @@ PrefetchData *createPrefetchData(char *domain, DynamicArray *buff);
 void termPrefetchData(PrefetchData *data);
 bool prefetchUrlCmp(PrefetchData *data, char *url);
 
+
+// This is the overall data list data structure.
+// It has the payload void* and a next pointer,
+// making it a singly linked list. The data
+// field will be set to one of the structs
+// above
 typedef struct DataList {
     void *data;
     struct DataList *next;
 } DataList;
 
+// These macros make it easy to typecast the
+// data functions above to these general uses
 #define CmpFunc bool (*)(void *, void *)
 #define TermFunc void (*)(void *)
 
+// To perform functionality like find, and delete, we need
+// other functionality inherent to the data. Because of this,
+// the user can specify their own comparision functions
+// and their own terminate functions.
+// The comparison will take 2 data points and return true
+// if they're the same. These are value and not reference
+// comparisons.
+// Terminate functions will just delete the data. They will
+// be responsible for freeing memory.
+
+// Each struct above has defined a create, compare, and delete
+// function. The create is typically used when calling add,
+// and the compare functions will be used in find and delete.
 DataList *addData(DataList *list, void *data);
 DataList *findData(DataList *list, bool (*cmp)(void *a, void *b), void *data);
 int dataListLength(DataList *list);
